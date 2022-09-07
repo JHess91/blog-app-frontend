156904335068b1ee4876c13bf023870f711ce8a3
Justin <justin.hessler@yahoo.com>
2022-09-06 21:51:45 -0400

    fixed update form

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,9 +4,11 @@ const AboutMe = () => {
       <h1>About Aquariums Around America</h1>
       <br />
       <img className="about-img" src="https://res-1.cloudinary.com/jnto/image/upload/w_2064,h_1300,c_fill,f_auto,fl_lossy,q_auto/v1648108505/okinawa/H_00376_001" alt="" />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptates voluptate debitis pariatur necessitatibus, sapiente temporibus ex beatae, dolores id quis maxime aut obcaecati tempore quam vel, libero quo? Quam!</p>
-      <br />
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis perspiciatis adipisci aspernatur, sequi commodi delectus quibusdam hic sit libero culpa quas facere alias dolores animi necessitatibus aperiam temporibus rerum saepe?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptates voluptate debitis pariatur necessitatibus, sapiente temporibus ex beatae, dolores id quis maxime aut obcaecati tempore quam vel, libero quo? Quam!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis perspiciatis adipisci aspernatur, sequi commodi delectus quibusdam hic sit libero culpa quas facere alias dolores animi necessitatibus aperiam temporibus rerum saepe?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quas, eius excepturi expedita itaque amet! Dicta earum, recusandae aliquid quam hic expedita voluptatem asperiores error eveniet similique quisquam numquam? Dolore!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti et exercitationem amet, nulla dolores maxime officia corporis quod, quas culpa eveniet inventore nostrum doloremque eum praesentium, sit libero perferendis! Aspernatur.
+      </p>
       <br />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quas, eius excepturi expedita itaque amet! Dicta earum, recusandae aliquid quam hic expedita voluptatem asperiores error eveniet similique quisquam numquam? Dolore!</p>
       <br />
